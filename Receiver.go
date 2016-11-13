083f9671f843c40fb9b48e8310014a7da83629d8
Tim Sayler <tsayler@gmail.com>
2016-11-13 00:07:36 -0800

    Added comments to the for infinite loop.

--- Receiver.go
+++ Receiver.go
@@ -20,7 +20,9 @@ func main() {
         fmt.Printf("Error %v\n", err)
     }
 	
-	//read and print incoming message
+	//read and print incoming messages
+	//till the end of time
+	//or ctrl-c
     for {
         _, err := lstn.Read(in)
         fmt.Printf("Packet sent at: %s", in)
